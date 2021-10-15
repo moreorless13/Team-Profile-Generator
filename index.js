680dd6b9106e532975e782e2527d6ba9a3aed36d
James Strande <strandejr@gmail.com>
2021-10-15 02:00:23 -0400

    added team class

--- index.js
+++ index.js
@@ -1,11 +1,85 @@
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
 
-const promptUser = () => {
-    return inquirer.prompt({
-        checkbox: 'input',
-        message: 'What is the role of the new employee?', 
-        name: 'role',
-        choices: ['Employee', 'Manager', 'Engineer', 'Intern'],
-    },);
-};
+let teamMembers = [];
 
+class Team {
+    constructor(){
+        this.teamSize = teamMembers.length;
+    }
+
+    manage() {
+        this.getManager();
+    }
+
+    getManager() {
+        return inquirer.prompt([
+            {
+                type: "input",
+                message: "Please enter Manager's Employee Id",
+                name: "id",
+            },
+            {
+                type: 'input',
+                message: "Please input Team Manager's Name: ",
+                name: "name",
+            },
+            {
+                type: "input",
+                message: "Please enter Manager's Email Address: ",
+                name: 'email',
+            },
+            {
+                type: 'input',
+                message: "Please Enter the Manager's Office Number: ",
+                name: 'officeNumber',
+            }
+        ])
+        .then(answers => {
+                let manager = new Manager(answers.id, answers.name, answers.email, answers.officeNumber);
+                teamMembers.push(manager)
+            })
+        .catch(err => console.error(err));
+    }
+
+    addAnotherTeamMember() {
+        inquirer.prompt([
+            {
+                type: 'confirm',
+                message: 'Add another team member?',
+                name: 'choice',
+            }
+        ])
+        .then(val => {
+            if (val.choice){
+                this.addTeamMember();
+            } else {
+                this.quit();
+            }
+        });
+    }
+    addTeamMember() {
+        
+    }
+
+    quit() {
+        console.log("\n Generating Team Page");
+        process.exit(0);
+    }
+
+
+}
+
+
+
+
+
+const init = () => {
+    console.log("Ignition...");
+    getManager();
+
+}

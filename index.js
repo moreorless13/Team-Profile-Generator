e0da09c909c74af1440ee4fe4ec32197d4f244fb
James Strande <strandejr@gmail.com>
2021-10-15 08:48:37 -0400

    prompts for all roles and adds each to team roster

--- index.js
+++ index.js
@@ -7,79 +7,141 @@ const fs = require('fs');
 
 let teamMembers = [];
 
-class Team {
-    constructor(){
-        this.teamSize = teamMembers.length;
-    }
+const getManager = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Please enter Manager's Employee Id",
+            name: "id",
+        },
+        {
+            type: 'input',
+            message: "Please input Team Manager's Name: ",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "Please enter Manager's Email Address: ",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "Please Enter the Manager's Office Number: ",
+            name: 'officeNumber',
+        }
+    ])
+    .then(answers => {
+            let manager = new Manager(answers.id, answers.name, answers.email, answers.officeNumber);
+            teamMembers.push(manager);
+            console.log(teamMembers);
+            addAnotherTeamMember();
+        })
+    .catch(err => console.error(err));
+};
 
-    manage() {
-        this.getManager();
-    }
-
-    getManager() {
-        return inquirer.prompt([
-            {
-                type: "input",
-                message: "Please enter Manager's Employee Id",
-                name: "id",
-            },
-            {
-                type: 'input',
-                message: "Please input Team Manager's Name: ",
-                name: "name",
-            },
-            {
-                type: "input",
-                message: "Please enter Manager's Email Address: ",
-                name: 'email',
-            },
-            {
-                type: 'input',
-                message: "Please Enter the Manager's Office Number: ",
-                name: 'officeNumber',
-            }
-        ])
-        .then(answers => {
-                let manager = new Manager(answers.id, answers.name, answers.email, answers.officeNumber);
-                teamMembers.push(manager)
-            })
-        .catch(err => console.error(err));
-    }
-
-    addAnotherTeamMember() {
-        inquirer.prompt([
-            {
-                type: 'confirm',
-                message: 'Add another team member?',
-                name: 'choice',
-            }
-        ])
-        .then(val => {
-            if (val.choice){
-                this.addTeamMember();
-            } else {
-                this.quit();
-            }
-        });
-    }
-    addTeamMember() {
-        
-    }
-
-    quit() {
-        console.log("\n Generating Team Page");
-        process.exit(0);
-    }
-
-
+const addTeamMember = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: "What is the new team member's role?",
+            name: 'role',
+            choices: ['Engineer', 'Intern'],
+        }
+    ])
+    .then(answer => {
+        if (answer.role == 'Engineer') {
+            return getEngineerQuestions();
+        } else {
+            return getInternQuestions();
+        }
+    })
 }
 
+const getEngineerQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Please enter Engineer's Employee Id",
+            name: "id",
+        },
+        {
+            type: 'input',
+            message: "Please input Engineer's Name: ",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "Please enter Engineer's Email Address: ",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "Please Enter the Engineers's GitHub Username: ",
+            name: 'username',
+        }
+    ])
+    .then(answers => {
+            let engineer = new Engineer(answers.id, answers.name, answers.email, answers.username);
+            teamMembers.push(engineer)
+            console.log(teamMembers);
+            addAnotherTeamMember();
+        })
+    .catch(err => console.error(err));
+}
 
+const getInternQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Please enter Intern's Employee Id",
+            name: "id",
+        },
+        {
+            type: 'input',
+            message: "Please input Intern's Name: ",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "Please enter Intern's Email Address: ",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "Please Enter the Intern's school: ",
+            name: 'school',
+        }
+    ])
+    .then(answers => {
+            let intern = new Intern(answers.id, answers.name, answers.email, answers.school);
+            teamMembers.push(intern);
+            console.log(teamMembers);
+            addAnotherTeamMember();
+        })
+    .catch(err => console.error(err));
+}
 
-
+const addAnotherTeamMember = () => {
+    inquirer.prompt([
+        {
+            type: 'confirm',
+            message: 'Add another team member?',
+            name: 'choice',
+        }
+    ])
+    .then(val => {
+        if (val.choice){
+            addTeamMember();
+        } else {
+            return;
+        }
+    });
+};
 
 const init = () => {
     console.log("Ignition...");
     getManager();
-
+    console.log(teamMembers);
 }
+
+init();
